2bf86cd01d573c47302d187f5b56f752d850d214
minphone <dev.minphon@gmail.com>
2018-11-27 15:52:48 +0630

    Init Project

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by MinPhone on 11/27/18.
+//  Copyright © 2018 MinPhone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
